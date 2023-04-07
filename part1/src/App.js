08febe0db53e8ad648fbb548730d2d8acd092e63
yuki <yuki@tamu.edu>
2023-04-07 17:40:29 -0500

    part 1

---
+++ part1/src/App.js
@@ -0,0 +1,24 @@
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const name = 'Peter';
+  const age = 10;
+
+  return (
+    <div>
+      <p>Greetings</p>
+      <Hello name='Maya' age={26+10}/>
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
+
+export default App
